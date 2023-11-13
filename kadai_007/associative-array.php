f363ac64cd347c40910efbea54a1cf1f1c9b30e3
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2023-11-13 14:31:35 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP_課題</title>
+
+</head>
+<body>
+   <p>
+     <?php
+       $syooping =['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+       print_r($syooping) ;
+
+      ?>
+   </p>  
+
+</body>
+</html>
