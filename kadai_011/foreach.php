ac804d1d32c46f340c3a31341ee0c3b74dcfd3a4
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2023-11-14 12:58:08 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題編</title>
+</head>
+<body>
+    <p>
+      <?php
+      $shopping = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
+      foreach($shopping as $key => $value){
+        echo "{$key}:{$value} <br>";
+      }
+
+
+       ?>
+    </p>
+</body>
+</html>
